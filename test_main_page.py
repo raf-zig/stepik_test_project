277edbe7832df939193dcc62c5a58e74bc79f5ee
unknown <ziravin@yandex.ru>
2022-08-20 13:48:17 +0500

    checking the availability of registration and login forms

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,25 @@
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) 
     page.open() 
-    page.go_to_login_page()  
-
-    #browser.get(link)
-    #login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    #login_link.click()
-
+    page.go_to_login_page() 
+   
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link) 
+    page.open() 
+    page.should_be_login_page()
+
+
+
+  
